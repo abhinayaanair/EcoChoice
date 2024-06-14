a82d9be9fc38c0441cd78ca7107ae3e2a0802f82
abhinayaanair <abhinayapersonal11@gmail.com>
2024-06-14 09:51:47 +0530

    updated app.js

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const initializeData = require("./init");
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/carbonEmissions', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/carbonEmissions').then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.error("Error connecting to MongoDB:", err);
