c481b578ce7284bf3ecd18c2ad45bee6fdcec54b
Abhinaya Nair <156507275+abhinayaanair@users.noreply.github.com>
2024-07-02 10:28:51 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function initializeData() {
           name: String,
           carbonEmission: Number,
           carbonEmission24: Number,
+          how:String,
         },
       ],
     });
